9a701384513bad8c448f24618a7a16652f9cc605
Hackbright Student <info@hackbrightacademy.com>
2014-03-07 11:57:15 -0800

    display first 5 users

--- judgement.py
+++ judgement.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+import model
+
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello world!"
+def index():
+    user_list = model.session.query(model.User).limit(5).all()
+    return render_template("user_list.html", users=user_list)
 
 if __name__ == "__main__":
     app.run(debug = True)
