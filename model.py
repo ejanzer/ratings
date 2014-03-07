93ba9d8611ea6d6db930d7231b254e19ab0bb291
Hackbright Student <info@hackbrightacademy.com>
2014-03-06 16:51:34 -0800

    init commit

--- model.py
+++ model.py
@@ -1,13 +1,64 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
 
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+
+
+
+# Movie:
+# id: integer
+# name: string
+# released_at: datetime
+# imdb_url: string
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(64))
+    released_at = Column(Date, nullable=True)
+    imdb_url = Column(String(64), nullable=True)
+
+# Rating:
+# id: integer
+# movie_id: integer
+# user_id: integer
+# rating: integer
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key=True)
+    movie_id = Column(Integer) # This needs to be a foreign key
+    user_id = Column(Integer) # This needs to be a foreign key
+    rating = Column(Integer)
 
 ### End class declarations
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
+
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -1,22 +1,61 @@
 import model
 import csv
+import datetime
 
 def load_users(session):
     # use u.user
-    pass
+    # open a file
+    with open('seed_data/u.user', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            words = row[0].split('|')
+            user = model.User(id=words[0], age=words[1], zipcode=words[4])
+            session.add(user)
+        session.commit()
 
 def load_movies(session):
-    # use u.item
-    pass
+
+# id: integer
+# name: string
+# released_at: datetime
+# imdb_url: string
+    with open('seed_data/u.item', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            words = row[0].split('|')
+            title = words[1]
+            title = title.split('(')[0].strip().decode("latin-1")
+            if (len(words) == 2):
+                movie = model.Movie(id=words[0], name=title)
+            elif (len(words) == 3):
+                movie = model.Movie(id=words[0], name=title, imdb_url=words[2])
+            else:
+                released_year = datetime.datetime.strptime(words[2], '%d-%b-%Y')
+                # print 'id :' + words[0]
+                # print 'released year: %s' % released_year
+                movie = model.Movie(id=words[0], name=title, released_at=released_year, imdb_url=words[3])
+            session.add(movie)
+        session.commit()
+
+# id = Column(Integer, primary_key=True)
+# movie_id = Column(Integer) # This needs to be a foreign key
+# user_id = Column(Integer) # This needs to be a foreign key
+# rating = Column(Integer)
 
 def load_ratings(session):
-    # use u.data
-    pass
+    with open('seed_data/u.data', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            ratings = row[0].split()
+            rating_obj = model.Rating(user_id=ratings[0], movie_id=ratings[1], rating=ratings[2])
+            session.add(rating_obj)
+        session.commit()
 
 def main(session):
-    # You'll call each of the load_* functions with the session as an argument
-    pass
-
+    load_ratings(session)
+    load_users(session)
+    load_movies(session)
+    
 if __name__ == "__main__":
     s= model.connect()
     main(s)
